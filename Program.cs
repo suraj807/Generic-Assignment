9d2c113405b45ae2c190caa94c6f7efba3013a6d
suraj807 <surajdev805@gmail.com>
2022-04-03 12:03:36 +0530

    find maximum of float

--- Program.cs
+++ Program.cs
@@ -12,14 +12,49 @@ namespace GenericsAssigment
         {
             Console.WriteLine("\nwelcome to Generic value");
 
-            Console.WriteLine("enter a first integer value");
-            int firstvalue = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter a second integer value");
-            int secondvalue = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter a third integer value");
-            int thirdvalue = Convert.ToInt32(Console.ReadLine());
-            int maxvalue = findmaximum.Findmaximuminteger(firstvalue, secondvalue, thirdvalue);
-            Console.WriteLine($"Maxium integer of ({firstvalue},{secondvalue},{thirdvalue}) is : {maxvalue}");
+            //Console.WriteLine("enter a first integer value");
+            //int firstvalue = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter a second integer value");
+            //int secondvalue = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter a third integer value");
+            //int thirdvalue = Convert.ToInt32(Console.ReadLine());
+
+            //int maxvalue = findmaximum.Findmaximuminteger(firstvalue, secondvalue, thirdvalue);
+            //Console.WriteLine($"Maxium integer of ({firstvalue},{secondvalue},{thirdvalue}) is : {maxvalue}");
+
+
+            Console.WriteLine("enter your choice");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    int first, second, third;
+                    Console.WriteLine("enter a first integer value");
+                    first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("enter a second integer value");
+                    second = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("enter a third integer value");
+                    third = Convert.ToInt32(Console.ReadLine());
+                    int maxvalue = findmaximum.Findmaximuminteger(first, second, third);
+                    Console.WriteLine($"Maxium integer of ({first},{second},{third}) is : {maxvalue}");
+                    break;
+
+
+                    case 2:
+                    float First, Second, Third;
+                    Console.WriteLine("enter a first integer value");
+                    First = float.Parse(Console.ReadLine());
+                    Console.WriteLine("enter a second integer value");
+                    Second = float.Parse(Console.ReadLine());
+                    Console.WriteLine("enter a third integer value");
+                    Third = float.Parse(Console.ReadLine());
+                    float floatmaxvalue = findmaximum.Findmaximumfloat(First, Second, Third);
+                    Console.WriteLine($"Maxium float of ({First},{Second},{Third}) is : {floatmaxvalue}");
+                    break;
+            }
+
+
+
 
             Console.ReadLine();
 
--- findmaximum.cs
+++ findmaximum.cs
@@ -14,18 +14,47 @@ namespace GenericsAssigment
                 firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0||
                 firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
             {
+                Console.WriteLine("first value is greater");
                 return firstvalue;
             }
             if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
                 secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
                 secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
             {
+                Console.WriteLine("second value is greater");
                 return secondvalue;
             }
             if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
                 thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
                 thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
             {
+                Console.WriteLine(" third value is greater");
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+
+        public static float Findmaximumfloat(float firstvalue, float secondvalue, float thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine("first value is greater");
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                Console.WriteLine("second value is greater");
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                Console.WriteLine("third value is greater");
                 return thirdvalue;
             }
             return firstvalue;
