aedd8206fc2b29c3c638061011d081b8eeae16e8
suraj807 <surajdev805@gmail.com>
2022-04-03 10:20:35 +0530

    find maximum of integer

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nwelcome to Generic value");
+
+            Console.WriteLine("enter a first integer value");
+            int firstvalue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a second integer value");
+            int secondvalue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a third integer value");
+            int thirdvalue = Convert.ToInt32(Console.ReadLine());
+            int maxvalue = findmaximum.Findmaximuminteger(firstvalue, secondvalue, thirdvalue);
+            Console.WriteLine($"Maxium integer of ({firstvalue},{secondvalue},{thirdvalue}) is : {maxvalue}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ findmaximum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssigment
+{
+    internal class findmaximum
+    {
+        public static int Findmaximuminteger(int firstvalue ,int secondvalue ,int thirdvalue)
+        {
+            if(firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue)> 0 ||
+                firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return firstvalue;
+            }
+            if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+                secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0)
+            {
+                return secondvalue;
+            }
+            if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+                thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0)
+            {
+                return thirdvalue;
+            }
+            return firstvalue;
+        }
+    }
+}
